06daa9823462d8a9abbc68123822b510538fe7c3
Tigran Gabrielyan <tigrangab@gmail.com>
2014-04-19 16:36:13 -0700

    Fixed issue where 401 was returned rather than 500 on db error

--- api/auth.go
+++ api/auth.go
@@ -5,18 +5,23 @@ import (
 	"github.com/jinzhu/gorm"
 	"net/http"
 	"ridesyncer/models"
+	"ridesyncer/utils"
 )
 
 const ApiTokenHeaderKey = "X-API-TOKEN"
 
 func AuthenticateUser(db *gorm.DB) martini.Handler {
-	return func(req *http.Request, c martini.Context) {
+	return func(res http.ResponseWriter, req *http.Request, c martini.Context) {
 		token := req.Header.Get(ApiTokenHeaderKey)
 		var authUser models.AuthUser
 		if token != "" {
 			var err error
-			if authUser.User, err = models.GetUserByToken(db, token); err == nil {
+			authUser.User, err = models.GetUserByToken(db, token)
+
+			if err == nil {
 				authUser.SetAuthenticated(true)
+			} else if err != gorm.RecordNotFound {
+				utils.HttpError(res, http.StatusInternalServerError)
 			}
 		}
 		c.Map(authUser)
@@ -27,7 +32,7 @@ func AuthenticateUser(db *gorm.DB) martini.Handler {
 func NeedsAuth(checkVerified bool) martini.Handler {
 	return func(res http.ResponseWriter, authUser models.AuthUser) {
 		if !authUser.IsAuthenticated() || (checkVerified && !authUser.EmailVerified) {
-			http.Error(res, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			utils.HttpError(res, http.StatusUnauthorized)
 		}
 	}
 }
